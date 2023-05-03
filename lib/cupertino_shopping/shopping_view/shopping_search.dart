c7f664d8f94fd47b31dafc0bed588aa39eef580b
Vivek <tailorvivekgec19@gmail.com>
2023-05-03 13:14:14 +0530

    iOS

--- lib/cupertino_shopping/shopping_view/shopping_search.dart
+++ lib/cupertino_shopping/shopping_view/shopping_search.dart
@@ -10,6 +10,20 @@ class Cupertino_Search extends StatefulWidget {
 class _Cupertino_SearchState extends State<Cupertino_Search> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(child: Center(child: Text("Shopping")));
+    return CupertinoPageScaffold(
+      child: Column(
+        children: [
+          Container(
+            height: 110,
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.bottomCenter,
+              child: SizedBox(height: 45,
+                child: CupertinoSearchTextField(
+
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
