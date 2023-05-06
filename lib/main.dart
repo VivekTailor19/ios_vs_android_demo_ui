7a00353c004b8ee2d1e37289737300ecbc7ba3b5
VIVEK <tailorvivekgec19@gmail.com>
2023-05-06 16:37:12 +0530

    ios android UI's

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,7 +15,10 @@ import 'android_home/google_play_store_ui/provider/google_play_provider.dart';
 import 'android_home/google_play_store_ui/app_view.dart';
 import 'android_home/google_play_store_ui/screens/homescreen/first_screen.dart';
 import 'ios_home/app_store/provider/appstore_provider.dart';
+import 'ios_home/app_store/view/appstore_home.dart';
 import 'ios_home/cupertino_shopping/shopping_provider/shopping_provider.dart';
+import 'ios_home/cupertino_shopping/shopping_view/shopping_home.dart';
+import 'ios_home/ios_home.dart';
 
 
 void main() {
@@ -40,31 +44,31 @@ void main() {
               // Platform.isAndroid != true ?
 
 
-              MaterialApp(
-                debugShowCheckedModeBanner: false,
-                initialRoute: "android_setting",
-                routes: {
-                    "ios_setting": (context) => iOS_Setting_UI(),
-                    "android_setting":(context) => Android_Settings(),
-                    "googleplay":(context) => First_Screen(),
-                    "appview":(context) => App_View_Android(),
-                  },
-              )
+              // MaterialApp(
+              //   debugShowCheckedModeBanner: false,
+              //   initialRoute: "ios_setting",
+              //   routes: {
+              //       "ios_setting": (context) => iOS_Setting_UI(),
+              //       "android_setting":(context) => Android_Settings(),
+              //       "googleplay":(context) => First_Screen(),
+              //       "appview":(context) => App_View_Android(),
+              //     },
+              // )
               // :
               //
-              // CupertinoApp(
-              //   debugShowCheckedModeBanner: false,
-              //   // initialRoute: "cupertinostore",
-              //   routes: {
-              //     "/":(context) => iOS_Home(),
-              //     "home":(context) => iOS_Home(),
-              //     "ios_setting": (context) => iOS_Setting_UI(),
-              //     "cupertinostore":(context) => Cupertino_Store(),
-              //     "appstore":(context) => App_Store_HomeScreen(),
-              //
-              //
-              //   },
-              // ),
+              CupertinoApp(
+                debugShowCheckedModeBanner: false,
+                 initialRoute: "appstore",
+                routes: {
+                  "/":(context) => iOS_Home(),
+                  "home":(context) => iOS_Home(),
+                  "ios_setting": (context) => iOS_Setting_UI(),
+                  "cupertinostore":(context) => Cupertino_Store(),
+                  "appstore":(context) => App_Store_HomeScreen(),
+
+
+                },
+              ),
 
             ),
 
